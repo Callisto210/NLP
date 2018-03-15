7b85a682f293a38c39a2504d225e69512e4bdcd1
Patryk Duda <patryk.d@10g.pl>
2018-03-15 15:54:46 +0100

    update

--- zad03/main.py
+++ zad03/main.py
@@ -5,21 +5,29 @@ from os.path import isfile, join
 import json
 import re
 import datetime
+import itertools
 
 import nltk
+from bs4 import BeautifulSoup
 
 def main():
-    filenames = [f for f in listdir('data/') if isfile(join('data/json', f)) and
+    only_letters = re.compile(r'[\W\d]+')
+    filenames = [f for f in listdir('data/') if isfile(join('data/', f)) and
                     re.match(r'judgments-\d+\.json', f) is not None]
     for name in filenames:
-        f = open(join('data/json', name), 'r')
+        f = open(join('data/', name), 'r')
         judgments = json.load(f)['items']
+        print(name)
         for judgment in judgments:
             try:
                 if datetime.datetime.strptime(judgment['judgmentDate'], "%Y-%m-%d").year == 2005:
-                    cleaned = nltk.clean_html(judgment['textContent'])
-                    tokens = nltk.word_tokenize(cleaned)
-                    print(tokens)
+                    cleaned = BeautifulSoup(judgment['textContent'], 'lxml').get_text()
+                    tokens = nltk.tokenize.word_tokenize(cleaned)
+                    wonum = list(itertools.filterfalse(only_letters.match, tokens))
+                    freq = nltk.FreqDist(wonum)
+                    for key, val in freq.items():
+                        print (str(key) + " : " + str(val))
+                    break
             except KeyError:
                 pass
         f.close()
